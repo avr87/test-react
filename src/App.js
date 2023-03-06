b9686cd9853e6a48be3697e3b7b81a5a0007d8ba
avr87 <annavanruiten@gmail.com>
2023-03-06 16:13:56 +0000

    displayed the weather for city

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Weather from "./Weather";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1> Jamani Hamjambo?</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <Weather city="Moshi"/>
+          
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+     Changing
+    
+  
       </header>
     </div>
   );
---
+++ src/Weather.js
@@ -0,0 +1,28 @@
+import { Audio } from "react-loader-spinner";
+import axios from "axios";
+
+export default function Weather(props) {
+  function showWeather(response) {
+    alert(
+      `The weather in ${props.city} is ${Math.round(response.data.main.temp)}`
+    );
+  }
+
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=1f853ed12117b75aa8323131f70900d7&units=metric`;
+  axios.get(apiUrl).then(showWeather);
+
+  return (<div>
+    <h4>
+  The weather in {props.city} is
+  </h4>
+     <Audio
+    height = "80"
+    width = "80"
+    radius = "9"
+    color = 'green'
+    ariaLabel = 'three-dots-loading'     
+    wrapperStyle
+    wrapperClass
+  />
+  </div>)
+}

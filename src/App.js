3f48951de3509d204992514640662b725964c7a9
avr87 <annavanruiten@gmail.com>
2023-03-08 16:35:56 +0000

    added the basic Weather-app lay-out and bootstrap

--- src/App.js
+++ src/App.js
@@ -3,15 +3,8 @@ import Weather from "./Weather";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1> Jamani Hamjambo?</h1>
-        <p>
-          <Weather city="Moshi" />
-        </p>
-      </header>
-    </div>
-  );
+          <Weather city="Moshi" />);
+  
 }
 
 export default App;
--- src/Weather.js
+++ src/Weather.js
@@ -1,28 +1,186 @@
-import { Audio } from "react-loader-spinner";
-import axios from "axios";
+
+import "./App.css";
 
 export default function Weather(props) {
-  function showWeather(response) {
-    alert(
-      `The weather in ${props.city} is ${Math.round(response.data.main.temp)}`
-    );
-  }
-
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=1f853ed12117b75aa8323131f70900d7&units=metric`;
-  axios.get(apiUrl).then(showWeather);
-
-  return (<div>
-    <h4>
-  The weather in {props.city} is
-  </h4>
-     <Audio
-    height = "80"
-    width = "80"
-    radius = "9"
-    color = 'aqua'
-    ariaLabel = 'three-dots-loading'     
-    wrapperStyle
-    wrapperClass
-  />
-  </div>)
+  let weatherData = {
+    city: "Moshi",
+    temperature: 26,
+    date: "Friday 17th of February 16:27",
+    description: "cloudy",
+    humidity: 70,
+    wind: 30,
+    imgUrl: "https://cdn-icons-png.flaticon.com/512/1779/1779940.png",
+  };
+  return (
+    <div className="App">
+      <div className="weather-app-wrapper">
+        <div className="container">
+          <div className="card">
+            <div className="card-body">
+              <form className="mb-4 search-form">
+                <div className="row">
+                  <div className="col-9">
+                    <input
+                      type="search"
+                      placeholder="Search city...."
+                      className="form-control city-input"
+                    />
+                  </div>
+                  <div className="col-3">
+                    <input
+                      className="btn btn-primary search-button"
+                      type="submit"
+                      value="Submit"
+                    />
+                  </div>
+                </div>
+              </form>
+              <h1> {weatherData.city}</h1>
+              <div className="clearfix weather-temperature">
+                <img
+                  src={weatherData.imgUrl}
+                  alt="weather-icon"
+                  className="float-left icon"
+                />
+                <strong className="temperature">
+                  {weatherData.temperature}
+                </strong>
+                <small>
+                  <a href="/" id="celsius-link" className="active celsius">
+                    ˚C
+                  </a>{" "}
+                  |
+                  <a href="/" id="fahrenheit-link" className="fahrenheit">
+                    ˚F
+                  </a>
+                </small>
+              </div>
+              <div className="row information">
+                <div class="col-6">
+                  <ul>
+                    <li className="description">{weatherData.description}</li>
+                    <li>
+                      Humidity:
+                      <span className="humidity"> {weatherData.humidity}</span>%
+                    </li>
+                    <li>
+                      Wind:
+                      <span className="windspeed"> {weatherData.wind}</span>
+                      mph
+                    </li>
+                  </ul>
+                </div>
+                <div className="col-6 info">
+                  <ul>
+                    <li>
+                      Last Updated:
+                      <span className="date"> {weatherData.date}</span>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+              <div className="weather-forecast forecast">
+                <div className="row">
+                  <div className="col-2">
+                    <div className="weather-forecast-day">Wed</div>
+                    <img
+                      src={weatherData.imgUrl}
+                      alt="weather-icon"
+                      className="icon-forecast"
+                    />
+                    <span className="weather-forecast-temperature-min forecast-min">
+                      11˚
+                    </span>
+                    <span className="weather-forecast-temperature-max forecast-max">
+                      13˚
+                    </span>
+                  </div>
+                  <div className="col-2">
+                    <div className="weather-forecast-day">Wed</div>
+                    <img
+                      src={weatherData.imgUrl}
+                      alt="weather-icon"
+                      className="icon-forecast"
+                    />
+                    <span className="weather-forecast-temperature-min forecast-min">
+                      11˚
+                    </span>
+                    <span className="weather-forecast-temperature-max forecast-max">
+                      13˚
+                    </span>
+                  </div>
+                  <div className="col-2">
+                    <div className="weather-forecast-day">Wed</div>
+                    <img
+                      src={weatherData.imgUrl}
+                      alt="weather-icon"
+                      className="icon-forecast"
+                    />
+                    <span className="weather-forecast-temperature-min forecast-min">
+                      11˚
+                    </span>
+                    <span className="weather-forecast-temperature-max forecast-max">
+                      13˚
+                    </span>
+                  </div>
+                  <div className="col-2">
+                    <div className="weather-forecast-day">Wed</div>
+                    <img
+                      src={weatherData.imgUrl}
+                      alt="weather-icon"
+                      className="icon-forecast"
+                    />
+                    <span className="weather-forecast-temperature-min forecast-min">
+                      11˚
+                    </span>
+                    <span className="weather-forecast-temperature-max forecast-max">
+                      13˚
+                    </span>
+                  </div>
+                  <div className="col-2">
+                    <div className="weather-forecast-day">Wed</div>
+                    <img
+                      src={weatherData.imgUrl}
+                      alt="weather-icon"
+                      className="icon-forecast"
+                    />
+                    <span className="weather-forecast-temperature-min forecast-min">
+                      11˚
+                    </span>
+                    <span className="weather-forecast-temperature-max forecast-max">
+                      13˚
+                    </span>
+                  </div>
+                  <div className="col-2">
+                    <div className="weather-forecast-day">Wed</div>
+                    <img
+                      src={weatherData.imgUrl}
+                      alt="weather-icon"
+                      className="icon-forecast"
+                    />
+                    <span className="weather-forecast-temperature-min forecast-min">
+                      11˚
+                    </span>
+                    <span className="weather-forecast-temperature-max forecast-max">
+                      13˚
+                    </span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="link-to-code">
+            <a
+              href="https://github.com/avr87/Anna-weather-app.git"
+              rel="noreferrer"
+              target="_blank"
+            >
+              Open-source code
+            </a>
+            <span className="name"> by Anna van Ruiten </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
